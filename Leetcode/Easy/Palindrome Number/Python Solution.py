93f9797bcb74d181069c45d25fd4e19e5826e86e
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2021-09-10 17:50:57 +0530

    Delete Leetcode directory

--- Leetcode/Easy/Palindrome Number/Python Solution.py
+++
@@ -1,3 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-       return True if(str(x) == str(x)[::-1]) else False
--- Leetcode/Easy/Two Sum/Python Solution.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        dic = {}
-        for i, num in enumerate(nums): 
-            if num in dic : return [dic[num], i] 
-            dic[target - num] = i
-​
-        
