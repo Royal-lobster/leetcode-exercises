dcfd2c1148cf208d840348c6f464b0a2ca92b980
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2021-09-16 20:48:41 +0530

    Time: 31 ms (11.60%), Space: 13.5 MB (24.19%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,19 +1,8 @@
 class Solution(object):
     def middleNode(self, head):
-        # find length of the linked list
-        current_node, list_length = head, 0
-        while current_node != None:
-            list_length += 1
-            current_node = current_node.next
-        
-        # find index of middle element
-        middle_index = list_length//2
-        
-        print([list_length, middle_index])
-        
-        # iterate through list till middle element and return it
-        current_node = head
-        for x in range(middle_index):
-            list_length += 1
-            current_node = current_node.next
-        return current_node
+        slow = fast = head
+        while fast and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+            
