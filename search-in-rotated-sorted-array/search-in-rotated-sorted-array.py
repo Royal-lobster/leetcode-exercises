f70ec453a6fe32ecccc8fafa8e86430d41413068
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2021-12-10 03:33:51 -0500

    Time: 32 ms (46.81%), Space: 13.8 MB (15.26%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,7 +1,32 @@
 class Solution(object):
     def search(self, nums, target):
-        for i,x in enumerate(nums):
-            if x == target:
-                return i
-        else:
-            return -1
+        
+        # INITALIZE LEFT RIGHT POINTERS
+        l, r = 0, len(nums)-1
+        
+        # START BINARY SEARCH
+        while l <= r: 
+            
+            # FIND MIDDLE INDEX
+            m = l + (r-l)// 2
+            
+            # TARGET IS THE MID ELEMENT
+            if nums[m] == target:
+                return m
+            
+            # IF MID IS IN LEFT PORTION OF PIVOT
+            if nums[l] <= nums[m]:
+                if target < nums[m] and nums[l] <= target:
+                    r = m - 1 # search right side of mid
+                else:
+                    l = m + 1 # search left side of mid
+                
+            # IF MID IS IN RIGHT PORTION OF PIVOT
+            else:
+                if nums[m] < target and target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        
+        # IF NO ELMENT FOUND
+        return -1                
