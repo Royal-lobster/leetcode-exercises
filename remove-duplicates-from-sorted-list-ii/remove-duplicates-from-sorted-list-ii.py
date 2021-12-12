bd4143d41dfd490be658513f856ab0bb33b3f8a2
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2021-12-12 10:20:54 -0500

    Time: 28 ms (80.43%), Space: 13.4 MB (69.11%) - LeetHub

--- remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -1,38 +1,27 @@
 class Solution(object):
     def deleteDuplicates(self, head):
-        
         if head == None:
             return head
-        
         p, l, r, d = None, head, head.next, False
-        
-        # TREVERSE THE LIST
         while(r != None):
-            print(p.val if p else p, [l.val, r.val])
-            # IF WE FOUND THE DUBLICATE NUMBER, MARK IT AND DELETE
             if l.val == r.val:
-                l.next = r.next # delete r from list
-                print("deleted ", r.val)
-                d, r = True, r.next # mark duplicate found and move r
-            
-            # NO DUBLICATE FOUND IN THIS ITERATION
+                l.next = r.next
+                d, r = True, r.next
             else:
-                # IF DUBLICATE FOUND IN PREVIOUS ITERATIONS, DELETE THAT HERE
                 if d:
                     if p == None : 
-                        head = l.next # delete l from list
+                        head = l.next
                     else: 
-                        p.next = l.next # delete l from list
+                        p.next = l.next
                     d = False
-                    print("deleted source", l.val)
                 else:
                     p = l
                 l, r = l.next, r.next
         if d:
             if p == None :
-                head = l.next # delete l from list
+                head = l.next
             else: 
-                p.next = l.next # delete l from list
+                p.next = l.next
         return head
 
                 
