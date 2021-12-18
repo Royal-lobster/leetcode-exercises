81c125f75b86ab341cbc0e007fa0276abacbcc74
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2021-12-18 12:05:38 +0530

    Time: 24 ms (38.46%), Space: 13.4 MB (88.15%) - LeetHub

--- backspace-string-compare/backspace-string-compare.py
+++ backspace-string-compare/backspace-string-compare.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def backspaceCompare(self, s, t):
         
-        # INITALIZE PARSED ARRAY
-        parsed = [[],[]]
-        
         # PARSE WORDS WITHOUT BACKSPACES
-        for i,w in enumerate([s, t]):
+        def parse(w):
+            temp = []
             for l in w:
                 if l != "#":
-                    parsed[i].append(l)
-                elif parsed[i]:
-                    parsed[i].pop()
+                    temp.append(l)
+                elif temp:
+                    temp.pop()
+            return temp
         
         # RETURN IF WORDS ARE SAME OR NOT
-        return parsed[0] == parsed[1]
+        return parse(s) == parse(t)
